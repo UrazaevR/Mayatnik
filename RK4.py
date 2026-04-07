9136f52998c98087db0ad3667105e167b1f139ff
Руслан <urazaewrus@yandex.ru>
2026-04-07 20:33:15 +0300

    Refactor Code

--- RK4.py
+++ RK4.py
@@ -1,92 +1,107 @@
-import math
-import matplotlib.pyplot as plt
-import os
+from PyQt6.QtCore import pyqtSignal, QThread
+from PyQt6.QtWidgets import QLabel
 import numpy as np
-
-end_time = 24 * 60 * 60
-
-g = 9.8
-m = 2
-d = 1
-I = 0.8
-R = 0.05
-b = 6 * math.pi * 1.81 * 10 ** -5 * R * d
+import math
+import os
 
 
-PSI0 = math.radians(30)
-PSI10 = 0
-w0_2 = m*g*d / I
-beta = b / I
+class RK4_Thread(QThread):
+    error_ocurred = pyqtSignal(Exception)
+    status_update = pyqtSignal(str)
+    plot_update = pyqtSignal(np.ndarray, np.ndarray)
 
-def read_floats_numpy(filename):
-    """Чтение групп с помощью numpy"""
-    all_values = np.fromfile(filename, dtype=np.float32)
+    def read_floats_numpy(self, filename):
+        """Чтение групп с помощью numpy"""
+        all_values = np.fromfile(filename, dtype=np.float32)
 
-    if len(all_values) % 11 != 0:
-        print(f"Предупреждение: количество значений ({len(all_values)}) не кратно 11")
+        if len(all_values) % 11 != 0:
+            print(f"Предупреждение: количество значений ({len(all_values)}) не кратно 11")
 
-    for i in range(0, len(all_values), 11):
-        yield all_values[i:i+11]
+        for i in range(0, len(all_values), 11):
+            yield all_values[i:i+11]
 
-def dfi1dt(fi1, fi, t):
-    return -w0_2 * math.sin(fi) - beta * fi1
+    def dfi1dt(self, fi1, fi, t):
+        w0_2 = self.m * self.g * self.d / self.I
+        beta = self.b / self.I
+        return -w0_2 * math.sin(fi) - beta * fi1
+    
+    def dfidt(self, fi1, fi, t):
+        return fi1
 
-def dfidt(fi1, fi, t):
-    return fi1
+    def set_params(self, canvas: 'FigureCanvas', stB: QLabel, END_TIME=24*60*60, h=0.01, g=9.8, m=2, d=1, I=0.8, R=0.05, PSI0=math.radians(30), PSI10=0):
+        self.canvas1 = canvas
+        self.stB = stB
 
+        self.END_TIME = END_TIME
+        self.h = h
+        self.g = g
+        self.m = m
+        self.d = d
+        self.I = I
+        self.R = R
+        self.PSI0 = PSI0
+        self.PSI01 = PSI10
+        self.b = 6 * math.pi * 1.81 * 10 ** -5 * R * d
 
-psi = np.array([])
-time = np.array([])
+    def run(self):
+        try:
+            self.status_update.emit('Поток вычислений запущен...')
+            
+            psi = np.array([])
+            time = np.array([])
 
-time2 = np.array([0])
-amp = np.array([PSI0])
+            time2 = np.array([0])
+            amp = np.array([self.PSI0])
 
-h = 0.01
-t = 0
-
-print('Начинаем считывать данные...')
-if os.path.exists('values.bin'):
-    for line in read_floats_numpy('values.bin'):
-        yi, y1i, t, m1, m2, m3, m4, k1, k2, k3, k4 = line
-        t += h
-        psi = np.append(psi, yi)
-        time = np.append(time, t)
-        print(f'\r{' ' * 50}\rАктуальное время: {t:.3f} с / {end_time} с', end='')
-        if t >= end_time:
-            break
-    data = open('values.bin', 'ab')
-    print('\nДанные прочитаны')
-else:
-    m1, m2, m3, m4, k1, k2, k3, k4 = 0, 0, 0, 0, 0, 0, 0, 0
-    yi = PSI0
-    y1i = PSI10
-    data = open('values.bin', 'wb')
-    print('Данных нет, начинаю расчет')
-
-
-while t < end_time:
-    y1i = y1i + (m1 + 2*m2 + 2*m3 + m4) / 6
-    yi = yi + (k1 + 2*k2 + 2*k3 + k4) / 6
-    psi = np.append(psi, yi)
-    time = np.append(time, t)
-    print(f'\rАктуальное время: {t:.3f} с / {end_time} с', end='')
-    if len(psi) > 2:
-        if (psi[-3] < psi[-2]) and (psi[-2] > psi[-1]):
-            amp = np.append(amp, psi[-1])
-            time2 = np.append(time2, time[-2])
-        elif (psi[-3] > psi[-2]) and (psi[-2] < psi[-1]):
-            time2[-1] = time[-2] - time2[-1]
-    m1 = h * dfi1dt(y1i, yi, t)
-    k1 = h * dfidt(y1i, yi, t)
-    m2 = h * dfi1dt(y1i + m1/2, yi + k1/2, t + h/2)
-    k2 = h * dfidt(y1i + m1/2, yi + k1/2, t + h/2)
-    m3 = h * dfi1dt(y1i + m2/2, yi + k2/2, t + h/2)
-    k3 = h * dfidt(y1i + m2/2, yi + k2/2, t + h/2)
-    m4 = h * dfi1dt(y1i + m3, yi + k3, t + h)
-    k4 = h * dfidt(y1i + m3, yi + k3, t + h)
-    np.array([yi, y1i, t, m1, m2, m3, m4, k1, k2, k3, k4], dtype=np.float32).tofile(data)
-    t = t + h
-print()
-
-plt.plot(time, list(math.degrees(x) for x in psi))
-plt.show()
+            self.status_update.emit('Начинаем считывать данные...')
+            i = 0
+            if os.path.exists('values.bin'):
+                for line in self.read_floats_numpy('values.bin'):
+                    i += 1
+                    yi, y1i, t, m1, m2, m3, m4, k1, k2, k3, k4 = line
+                    t += self.h
+                    psi = np.append(psi, yi)
+                    time = np.append(time, t)
+                    self.status_update.emit(f'Актуальное время: {t:.3f} с / {self.END_TIME} с')
+                    if i % 1000 == 0:
+                        self.plot_update.emit(time, psi)
+                    if t >= self.END_TIME:
+                        break
+                data = open('values.bin', 'ab')
+                self.status_update.emit('Данные прочитаны')
+            else:
+                m1, m2, m3, m4, k1, k2, k3, k4 = 0, 0, 0, 0, 0, 0, 0, 0
+                yi = self.PSI0
+                y1i = self.PSI10
+                data = open('values.bin', 'wb')
+                self.status_update.emit('Данных нет, начинаю расчет')
+            
+            while t < self.END_TIME:
+                i += 1
+                y1i = y1i + (m1 + 2*m2 + 2*m3 + m4) / 6
+                yi = yi + (k1 + 2*k2 + 2*k3 + k4) / 6
+                psi = np.append(psi, yi)
+                time = np.append(time, t)
+                self.status_update.emit(f'Актуальное время: {t:.3f} с / {self.END_TIME} с')
+                if len(psi) > 2:
+                    if (psi[-3] < psi[-2]) and (psi[-2] > psi[-1]):
+                        amp = np.append(amp, psi[-1])
+                        time2 = np.append(time2, time[-2])
+                    elif (psi[-3] > psi[-2]) and (psi[-2] < psi[-1]):
+                        time2[-1] = time[-2] - time2[-1]
+                m1 = self.h * self.dfi1dt(y1i, yi, t)
+                k1 = self.h * self.dfidt(y1i, yi, t)
+                m2 = self.h * self.dfi1dt(y1i + m1/2, yi + k1/2, t + self.h/2)
+                k2 = self.h * self.dfidt(y1i + m1/2, yi + k1/2, t + self.h/2)
+                m3 = self.h * self.dfi1dt(y1i + m2/2, yi + k2/2, t + self.h/2)
+                k3 = self.h * self.dfidt(y1i + m2/2, yi + k2/2, t + self.h/2)
+                m4 = self.h * self.dfi1dt(y1i + m3, yi + k3, t + self.h)
+                k4 = self.h * self.dfidt(y1i + m3, yi + k3, t + self.h)
+                np.array([yi, y1i, t, m1, m2, m3, m4, k1, k2, k3, k4], dtype=np.float32).tofile(data)
+                if i % 1000 == 0:
+                    self.plot_update.emit(time, psi)
+                t = t + self.h
+            self.status_update.emit('Вычисления завершены')
+            return super().run()
+        except Exception as e:
+            self.error_ocurred.emit(e)
--- main.py
+++ main.py
@@ -13,37 +13,7 @@ from matplotlib.axes import Axes
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
-
-END_TIME = 24*60*60
-h = 0.01
-
-g = 9.8
-m = 2
-d = 1
-I = 0.8
-R = 0.05
-b = 6 * math.pi * 1.81 * 10 ** -5 * R * d
-
-PSI0 = math.radians(30)
-PSI10 = 0
-w0_2 = m*g*d / I
-beta = b / I
-
-def read_floats_numpy(filename):
-    """Чтение групп с помощью numpy"""
-    all_values = np.fromfile(filename, dtype=np.float32)
-
-    if len(all_values) % 11 != 0:
-        print(f"Предупреждение: количество значений ({len(all_values)}) не кратно 11")
-
-    for i in range(0, len(all_values), 11):
-        yield all_values[i:i+11]
-
-def dfi1dt(fi1, fi, t):
-    return -w0_2 * math.sin(fi) - beta * fi1
-
-def dfidt(fi1, fi, t):
-    return fi1
+from RK4 import RK4_Thread
 
 
 class MplCanvas(FigureCanvas):
@@ -51,79 +21,6 @@ class MplCanvas(FigureCanvas):
         fig = Figure(figsize=(width, height))
         self.axes = fig.add_subplot(111)
         super().__init__(fig)
-
-
-class RK4_Thread(QThread):
-    error_ocurred = pyqtSignal(Exception)
-    status_update = pyqtSignal(str)
-    plot_update = pyqtSignal(np.ndarray, np.ndarray)
-
-    def set_params(self, canvas: FigureCanvas, stB: QLabel):
-        self.canvas1 = canvas
-        self.stB = stB
-
-    def run(self):
-        try:
-            self.status_update.emit('Поток вычислений запущен...')
-            
-            psi = np.array([])
-            time = np.array([])
-
-            time2 = np.array([0])
-            amp = np.array([PSI0])
-
-            print('Начинаем считывать данные...')
-            i = 0
-            if os.path.exists('values.bin'):
-                for line in read_floats_numpy('values.bin'):
-                    i += 1
-                    yi, y1i, t, m1, m2, m3, m4, k1, k2, k3, k4 = line
-                    t += h
-                    psi = np.append(psi, yi)
-                    time = np.append(time, t)
-                    self.status_update.emit(f'{' ' * 50}\rАктуальное время: {t:.3f} с / {END_TIME} с')
-                    if i % 1000 == 0:
-                        self.plot_update.emit(time, psi)
-                    if t >= END_TIME:
-                        break
-                data = open('values.bin', 'ab')
-                self.status_update.emit('Данные прочитаны')
-            else:
-                m1, m2, m3, m4, k1, k2, k3, k4 = 0, 0, 0, 0, 0, 0, 0, 0
-                yi = PSI0
-                y1i = PSI10
-                data = open('values.bin', 'wb')
-                self.status_update.emit('Данных нет, начинаю расчет')
-            
-            while t < END_TIME:
-                i += 1
-                y1i = y1i + (m1 + 2*m2 + 2*m3 + m4) / 6
-                yi = yi + (k1 + 2*k2 + 2*k3 + k4) / 6
-                psi = np.append(psi, yi)
-                time = np.append(time, t)
-                self.status_update.emit(f'Актуальное время: {t:.3f} с / {END_TIME} с')
-                if len(psi) > 2:
-                    if (psi[-3] < psi[-2]) and (psi[-2] > psi[-1]):
-                        amp = np.append(amp, psi[-1])
-                        time2 = np.append(time2, time[-2])
-                    elif (psi[-3] > psi[-2]) and (psi[-2] < psi[-1]):
-                        time2[-1] = time[-2] - time2[-1]
-                m1 = h * dfi1dt(y1i, yi, t)
-                k1 = h * dfidt(y1i, yi, t)
-                m2 = h * dfi1dt(y1i + m1/2, yi + k1/2, t + h/2)
-                k2 = h * dfidt(y1i + m1/2, yi + k1/2, t + h/2)
-                m3 = h * dfi1dt(y1i + m2/2, yi + k2/2, t + h/2)
-                k3 = h * dfidt(y1i + m2/2, yi + k2/2, t + h/2)
-                m4 = h * dfi1dt(y1i + m3, yi + k3, t + h)
-                k4 = h * dfidt(y1i + m3, yi + k3, t + h)
-                np.array([yi, y1i, t, m1, m2, m3, m4, k1, k2, k3, k4], dtype=np.float32).tofile(data)
-                if i % 1000 == 0:
-                    self.plot_update.emit(time, psi)
-                t = t + h
-            self.status_update.emit('Вычисления завершены')
-            return super().run()
-        except Exception as e:
-            self.error_ocurred.emit(e)
 
 
 class Window(QWidget):
